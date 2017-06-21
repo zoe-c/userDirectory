99106a4ef4a03e26970b593951b20e7b5b4b49df
zoe-c <find.zoec@gmail.com>
2017-06-21 18:02:28 -0400

    the index now includes all bots. styles needed.

--- index.js
+++ index.js
@@ -9,52 +9,11 @@ app.set('views', './views');
 app.set('view engine', 'mustache');
 
 
+const bots = data.users;
 app.get('/', function (req, res) {
-  res.render('index',
-  { img: data.users[0].avatar,
-    name: data.users[0].name,
-    job: data.users[0].job,
-    company: data.users[0].company
-  })
+   res.render('index', {bots});
 });
 
-
-// //NOTE: This rendered everything as TAGS on the page (with the appropriate data pulled for each bot....)...
-// // now how to figure out how to pass this to the index.mustache and then back to the app to render the index as done below?
-// var bots = data.users;
-// function renderBots() {
-//    return `
-//       ${bots.map(bot =>
-//          `<ul class="botcontainer">
-//           <li>
-//             <img src="${bot.avatar}" alt="oops!-caught-in-the-cloud">
-//           </li>
-//           <li>${bot.name}</li>
-//           <li>${bot.job}</li>
-//           <li>${bot.company}</li>
-//           </ul>
-//           `).join('')}
-//
-//    `;
-// }
-
-// app.get('/', function (req, res) {
-//    res.render('index', {users: data.users})
-//       <ul class="botcontainer">
-//        <li>
-//          <img src={user.avatar} alt="oops!-caught-in-the-cloud">
-//          </li>
-//          <li>{bot.name}</li>
-//          <li>{bot.job}</li>
-//          <li>{bot.company}</li>
-//          </ul>
-//
-// })
-//
-//
-// app.get('/', function (req, res) {
-//   res.render('index', renderBots());
-// });
 
 app.listen(3000, function () {
   console.log('App Start Successful!')
