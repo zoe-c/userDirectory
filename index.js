4cefefc637178653fbfe64a7d01cd6fc7e533baa
zoe-c <find.zoec@gmail.com>
2017-06-21 19:19:37 -0400

    connect stylesheet. basic styles.

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const app = express();
 const mustacheExpress = require('mustache-express');
 const data = require('./data.js');
 
-
+// scaffold
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
+// styles
+app.use(express.static('public'));
 
+// data   content
 const bots = data.users;
 app.get('/', function (req, res) {
    res.render('index', {bots});
